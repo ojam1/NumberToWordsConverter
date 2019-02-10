82183dc08dbc69b2fd1e9ef96ed96387047f70d0
Olly <oranson1@gmail.com>
2019-02-10 11:27:19 +0000

    MainPage components saves numbers to redux store

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class MainPage extends Component {
+import { storeNumbers } from '../actions/numbersToProcess';
+import { allWords } from '../actions/words';
+
+class MainPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      numbers: ''
     };
   }
 
   render() {
+    const { storeNumbers } = this.props;
+    const { numbers } = this.state
+
     return (
       <div className='d-flex align-items-center flex-column'>
-        <h1 className='display-4 mt-1 pt-1'>{this.state.text}</h1>
+        <h1 className='display-4 mt-1 pt-1'>{numbers}</h1>
         <div className='d-flex flex-row'>
           <div className='d-flex flex-column pb-1'>
             <button
               className='btn btn-outline-primary m-1 mw-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '1' })}
+              onClick={() => this.setState({ numbers: numbers + '1' })}
             >
               1
             </button>
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '4' })}
+              onClick={() => this.setState({ numbers: numbers + '4' })}
             >
               4 ghi
             </button>
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '7' })}
+              onClick={() => this.setState({ numbers: numbers + '7' })}
             >
               7 pqrs
             </button>
@@ -39,57 +46,76 @@ export default class MainPage extends Component {
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '2' })}
+              onClick={() => this.setState({ numbers: numbers + '2' })}
             >
               2 abc
             </button>
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '5' })}
+              onClick={() => this.setState({ numbers: numbers + '5' })}
             >
               5 jkl
             </button>
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '8' })}
+              onClick={() => this.setState({ numbers: numbers + '8' })}
             >
               8 tuv
             </button>
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '0' })}
+              onClick={() => this.setState({ numbers: numbers + '0' })}
             >
               0
+            </button>
+            <button
+              type='button'
+              className='btn btn-outline-primary m-1 w-33 rounded-circle'
+              onClick={() => this.setState({ numbers: '' })}
+            >
+              clear
             </button>
           </div>
           <div className='d-flex flex-column pb-1'>
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '3' })}
+              onClick={() => this.setState({ numbers: numbers + '3' })}
             >
               3 def
             </button>
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '6' })}
+              onClick={() => this.setState({ numbers: numbers + '6' })}
             >
               6 mno
             </button>
             <button
               type='button'
               className='btn btn-outline-primary m-1 w-33 rounded-circle'
-              onClick={() => this.setState({ text: this.state.text + '9' })}
+              onClick={() => this.setState({ numbers: numbers + '9' })}
             >
               9 wxyz
             </button>
           </div>
         </div>
+        <button
+          type='button'
+          className='btn btn-primary m-5 p-2 w-75'
+          onClick={() => storeNumbers(numbers)}
+        >
+          Submit
+        </button>
       </div>
     );
   }
 }
+
+export default connect(
+  null,
+  { storeNumbers }
+)(MainPage);
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,13 @@ import thunk from 'redux-thunk';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import wordReducer from '../reducers/words';
+import numbersToProcess from '../reducers/numbersToProcess'
 
 export default () => {
   const store = createStore(
     combineReducers({
-      word: wordReducer
+      word: wordReducer,
+      numbersToProcess: numbersToProcess
     }),
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__(),
